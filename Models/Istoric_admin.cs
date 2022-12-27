cbfedc150ea6834e5bdf3cbca3c49baf6eeed2b1
predaalex <preda.alex30@yahoo.com>
2022-12-27 14:39:27 +0200

    Am creat baza de date si modelele.

---
+++ Models/Istoric_admin.cs
@@ -0,0 +1,9 @@
+﻿namespace WikipediaDAW.Models
+{
+    public class Istoric_admin
+    {
+        public int Id { get; set; }
+        public Utilizator Utilizator { get; set; }
+        public String Actiune { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WikipediaDAW.ContextModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<UtilizatorContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("db")));
+
 
 var app = builder.Build();
 
