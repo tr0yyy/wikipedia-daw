79fd9b944ee20e76353a9ecec4e725629ea298c9
anicoi <anicoi@bitdefender.com>
2022-12-27 18:40:42 +0200

    wip

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public articole: ArticolInterface[] = [];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<ArticolInterface[]>(baseUrl + 'articol').subscribe(result => {
+      console.log("test");
+      console.log(result);
+      this.articole = result;
+    }, error => console.error(error));
+  }
+}
+
+
+interface ArticolInterface {
+  Id: number;
+  domeniu: string;
+  titlu: string;
+  autor_creare: object;
+  data_adaugarii: object;
+  continut: string;
+  protejat: boolean;
 }
