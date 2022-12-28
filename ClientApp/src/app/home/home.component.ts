89811f8002183a789e3b4f591888f04132fae035
anicoi <anicoi@bitdefender.com>
2022-12-28 16:23:38 +0200

    main-page beta

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,19 +1,39 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  public articole: ArticolInterface[] = [];
+  articole: ArticolInterface[] = [];
+  public articoleFiltrate: ArticolInterface[] = [];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<ArticolInterface[]>(baseUrl + 'articol').subscribe(result => {
-      console.log("test");
-      console.log(result);
-      this.articole = result;
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, public datepipe: DatePipe) {
+    http.get<ArticolInterface[]>(baseUrl + 'articol').subscribe(async result => {
+      this.articole = await result;
+      for (var articol of this.articole) {
+        articol.link = this.generateLinkFromTitle(baseUrl, articol.titlu)
+        articol.data_adaugarii = this.datepipe.transform(articol.data_adaugarii as string, 'dd/MM/yyyy') as string
+      }
+      this.articoleFiltrate = this.articole;
     }, error => console.error(error));
+  }
+
+  filterValues(value: any) {
+    value = value.target.value
+    value = value.toLowerCase();
+    if (value == "") {
+      this.articoleFiltrate = this.articole;
+    } else {
+      this.articoleFiltrate = this.articoleFiltrate.filter((articol) => articol.titlu.toLowerCase().includes(value))
+    }
+  }
+
+  generateLinkFromTitle(baseUrl: string, title: string) {
+    let link: string = baseUrl + "articol/" + title;
+    return link;
   }
 }
 
@@ -23,7 +43,8 @@ interface ArticolInterface {
   domeniu: string;
   titlu: string;
   autor_creare: object;
-  data_adaugarii: object;
+  data_adaugarii: string;
   continut: string;
   protejat: boolean;
+  link: string;
 }
