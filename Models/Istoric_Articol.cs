21947d42682bc023377b326f4278f80accb1ce88
anicoi <alexandru.nicoi@s.unibuc.ro>
2023-01-11 01:25:06 +0200

    create article + revert

---
+++ Models/Istoric_Articol.cs
@@ -0,0 +1,15 @@
+﻿namespace WikipediaDAW.Models
+{
+    public class Istoric_Articol
+    {
+
+        public int Id { get; set; }
+        
+        public Articol articol { get; set; }
+
+        public string continut_vechi { get; set; }
+
+        public DateTime data_editarii { get; set; }
+
+    }
+}
